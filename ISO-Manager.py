bbb84762d36feb9a0600a7b943223b283cafbaa5
Azazel55605 <122084315+Azazel55605@users.noreply.github.com>
2024-09-15 21:49:38 +0200

    simultaneous download capabilities
    fixed issues with ubuntu

--- ISO-Manager.py
+++ ISO-Manager.py
@@ -7,7 +7,6 @@ from concurrent.futures import ThreadPoolExecutor
 import signal
 from functools import partial
 from threading import Event
-from typing import Iterable
 from urllib.request import urlopen
 from simple_term_menu import TerminalMenu
 
@@ -46,7 +45,7 @@ signal.signal(signal.SIGINT, handle_sigint)
 
 def copy_url(task_id: TaskID, url: str, path: str) -> None:
     """Copy data from a url to a local file."""
-    progress.console.log(f"Requesting {url}")
+    # progress.console.log(f"Requesting {url}")
     response = urlopen(url)
     # This will break if the response doesn't contain content length
     progress.update(task_id, total=int(response.info()["Content-length"]))
@@ -57,19 +56,19 @@ def copy_url(task_id: TaskID, url: str, path: str) -> None:
             progress.update(task_id, advance=len(data))
             if done_event.is_set():
                 return
-    progress.console.log(f"Downloaded {path}")
+    # progress.console.log(f"Downloaded {path}")
 
 
 def download(urls, dest_dir: str):
     """Download multiple files to the given directory."""
 
     with progress:
-        with ThreadPoolExecutor(max_workers=4) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             for url in urls:
-                filename = url.split("/")[-1]
+                filename = url[0].split("/")[-1]
                 dest_path = os.path.join(dest_dir, filename)
                 task_id = progress.add_task("download", filename=filename, start=False)
-                pool.submit(copy_url, task_id, url, dest_path)
+                pool.submit(copy_url, task_id, url[0], dest_path)
 
 def read_conf(name):
     with open(f'./Modules/{name}.conf', 'r') as file:
@@ -79,9 +78,6 @@ def read_conf(name):
 
 
 def create_download_link(server, cwd, filename):
-    print(server)
-    print(cwd)
-    print(filename)
     link = f"https://{server}{cwd}/{filename}"
     return link
 
@@ -100,7 +96,7 @@ def ftp_traverse(os_name):
     entries = ftp.nlst()
     return_files = []
     match os_name:
-        case "ubuntu":
+        case "ubuntu" | "ubuntu-server":
             regex = re.compile("[0-9][0-9].[0-9][0-9]?.?[0-9]?[0-9]")
             versions = []
             files = []
@@ -112,9 +108,8 @@ def ftp_traverse(os_name):
             for new_entry in newest_entries:
                 if "-" in new_entry:
                     if (versions[-1] == (new_entry.split('-')[1])) and (new_entry.split(".")[-1] == "iso"):
-                        print(new_entry)
-
                         files.append(new_entry)
+
             return_files.append(create_download_link(server, f"{cwd}/{versions[-1]}", files[int(options)]))
 
         case "arch":
@@ -123,24 +118,22 @@ def ftp_traverse(os_name):
             for entry in entries:
                 if "-" in entry:
                     if (regex.match(entry.split('-')[1])) and (entry.split(".")[-1] == "iso"):
-                        print(entry)
                         files.append(entry)
+
             return_files.append(create_download_link(server, cwd, files[0]))
     return return_files
 
 
-
-
 def main():
     os_list = {
-        "ubuntu"
+        "ubuntu",
+        "ubuntu-server",
     }
 
-
+    file = []
     for os_entry in os_list:
-        file = ftp_traverse(os_entry)
-        print(file)
-        download(file, "./")
+        file.append(ftp_traverse(os_entry))
+    download(file, "./")
 
 if __name__ == "__main__":
     main()
